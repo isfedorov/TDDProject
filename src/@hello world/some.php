ffa7ddc809072001741fbf678a60e061dd26f583
Ivan Fedorov <ivan.fedorov@jetbrains.com>
2018-09-17 19:14:27 +0300

    test 3

--- src/@hello world/some.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: ifedorov
- * Date: 17.09.18
- * Time: 16:13
- */
---
+++ src/@hello world/some.tmp.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+
+/**
+ * Class Cssss
+ */
+class Cssss
+{
+    public function m()
+    {
+        foreach ([1,2,3] as $this) {
+        }
+    }
+}
